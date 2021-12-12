c3eb1dc15b030b8359719faa08eb2fe12de46e7a
Khushboo <guptakhushboo81537@gmail.com>
2021-12-12 13:57:48 +0530

    Added Swipe Cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:swipe_cards/swipe_cards.dart';
 import 'package:tinder_app/widgets/appBar.dart';
 import 'package:tinder_app/widgets/bottomBar.dart';
+import 'package:tinder_app/utils/constants.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,6 +29,23 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<SwipeItem> _swipeItems = <SwipeItem>[];
+  MatchEngine? _matchEngine;
+  List<String> name = ['Shweta', 'Rahul', 'Ananya', 'Jackie'];
+
+  @override
+  void initState() {
+    for (int i = 0; i < name.length; i++) {
+      _swipeItems.add(SwipeItem(
+          content: Content(name[i]),
+          likeAction: () {},
+          nopeAction: () {},
+          superlikeAction: () {}));
+    }
+    _matchEngine = MatchEngine(swipeItems: _swipeItems);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,11 +54,37 @@ class _HomeState extends State<Home> {
           children: [
             SizedBox(height: 70),
             TopBar(),
-            Expanded(child: Container()),
+            Expanded(
+                child: Container(
+              child: SwipeCards(
+                  matchEngine: _matchEngine,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      decoration: BoxDecoration(
+                        image:
+                            DecorationImage(image: AssetImage(images[index])),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Column(
+                        children: [Text(name[index])],
+                      ),
+                    );
+                  },
+                  onStackFinished: () {
+                    return ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('The list is over')));
+                  }),
+            )),
             BottomBar(),
           ],
         ),
       ),
     );
   }
+}
+
+class Content {
+  final String? text;
+  Content(this.text);
 }
---
+++ lib/utils/constants.dart
@@ -0,0 +1,6 @@
+List images = [
+  'assets/shweta.png',
+  'assets/rahul.png',
+  'assets/ananya.png',
+  'assets/jackie.png',
+];
